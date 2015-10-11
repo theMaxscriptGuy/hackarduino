43f5cfb07bfc96e72cc128ec6c2cd9f216e5768d
theMaxscriptGuy <videep@live.com>
2015-10-11 17:42:03 +0530

    code to change the color of the webpage.

--- hackarduino.js
+++ hackarduino.js
@@ -1,35 +1,48 @@
+//api.addFiles('/sky_color_map.json', 'server', {isAsset: true});
+
 if (Meteor.isClient) {
 
-
+    Session.setDefault('clr',0);
+    
+    //code to listen to the published vales from the server:
+    
   Meteor.subscribe("tasks",
   {
     onReady: function(){
       console.log(Tasks.find().data);
-
     }
   }
 )
   Meteor.setInterval(function()
   {
     //console.log("Hello World!");
-    console.log(Tasks.findOne().name)
-  },5000);
+                     console.log(Tasks.findOne().name);
+                     //added code to update the paragraph of the color:
+                     document.body.style.backgroundColor = Tasks.findOne().name;
+  },50);
+    
 }
+
+
+
 Tasks = new Mongo.Collection("tasks");
+
+
 if (Meteor.isServer) {
   Tasks.remove({});
 
-  var sky_colors;
-  var fs = Npm.require('fs');
-    fs.readFileSync('/public/sky_color_map.json', 'utf8', function (err, data) {
-        if (err) {
-            console.log('Error: ' + err);
-            return;
-        }
-
-        sky_colors = JSON.parse(data);
-        console.log(sky_colors);
-    });
+  var sky_colors = JSON.parse(Assets.getText("companion/a.json"));
+//  var fs = Npm.require('fs');
+//    fs.readFileSync('sky_color_map.json', 'utf8', function (err, data) {
+//        if (err) {
+//            console.log('Error: ' + err);
+//            return;
+//        }
+//
+//        sky_colors = JSON.parse(data);
+//        console.log(sky_colors);
+//    });
+    
 
 //connect to serial port:
 //  var serialPort = new SerialPort.SerialPort("/dev/tty.usbmodem1421", {
@@ -50,7 +63,7 @@ if (Meteor.isServer) {
       Tasks.remove({});
       console.log(sky_colors[sData]);
       Tasks.upsert({_id: 0}, {name:sky_colors[sData]});
-  }, 1000
+  }, 50
   );
   Meteor.publish("tasks", function () {
          return Tasks.find();
