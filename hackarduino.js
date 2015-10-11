f69fbcc7a70688cb406aaadc1309d00333a67065
theMaxscriptGuy <videep@live.com>
2015-10-11 23:29:58 +0530

    adding final code.:

--- hackarduino.js
+++ hackarduino.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var globalCommand = "A1off";
 var start,stop;
 var sky_colors;
@@ -130,7 +131,68 @@ if (Meteor.isServer) {
                             Tasks.upsert({_id: 0}, {name:sData});
                        },200
                        );
+=======
+var sky_colors;
 
+Meteor.startup(function() {
+  sky_colors = JSON.parse(Assets.getText('sky_color_map.json'));
+});
+
+if (Meteor.isClient) {
+
+
+  Meteor.subscribe("tasks", {
+    onReady: function(){
+      var received_color = Tasks.find().data;
+      console.log(received_color);
+      Template.body.helpers({
+          bgcolor: received_color
+        });
+    }
+  });
+
+  Meteor.setInterval(function()
+  {
+    //console.log("Hello World!");
+    console.log(Tasks.findOne().name)
+  },5000);
+}
+Tasks = new Mongo.Collection("tasks");
+if (Meteor.isServer) {
+  Tasks.remove({});
+>>>>>>> 3864a67dfa8c6fddc91fa37867964f8413834313
+
+    // fs.readFileSync('/sky_color_map.json', 'utf8', function (err, data) {
+    //     if (err) {
+    //         console.log('Error: ' + err);
+    //         return;
+    //     }
+
+    //     sky_colors = JSON.parse(data);
+    //     console.log(sky_colors);
+    // });
+
+//connect to serial port:
+//  var serialPort = new SerialPort.SerialPort("/dev/tty.usbmodem1421", {
+//                                                baudrate: 9600,
+//                                                parser: SerialPort.parsers.readline('\r\n')
+//                                               });
+////Get the data from the port:
+//
+//  serialPort.on('data', Meteor.bindEnvironment(function(data) {
+//  console.log('message ' + data);
+//  Tasks.remove({});
+//  Tasks.upsert({_id: 0}, {name:data});
+//  serialPort.write("0101");
+//  }));
+  var sData = 0;
+  Meteor.setInterval(function() {
+      sData+=1;
+      Tasks.remove({});
+      console.log(sky_colors[sData]);
+      Tasks.upsert({_id: 0}, {name:sky_colors[sData]});
+  }, 1000
+  );
   Meteor.publish("tasks", function () {
          return Tasks.find();
    });
